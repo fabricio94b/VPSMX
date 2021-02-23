f8dffba1874a94da5370be0ae7d7c7e00a2289ad
fabricio94b <60627225+fabricio94b@users.noreply.github.com>
2021-02-23 13:43:43 -0300

    UP

--- instalador/VPS-MX.sh
+++ instalador/VPS-MX.sh
@@ -13,7 +13,7 @@ tar -xf pytransform.tar > /dev/null 2>&1
 rm -rf pytransform.tar > /dev/null 2>&1
 cd
 echo -ne '\e[97mCARGANDO:\033[1;37m\e[93m ######################         (75%)\r'
-wget https://raw.githubusercontent.com/lacasitamx/VPSMX/master/instalador//VPS-MX.py -O /usr/bin/VPS-MX &> /dev/null
+wget https://raw.githubusercontent.com/fabricio94b/VPSMX/master/instalador//VPS-MX.py -O /usr/bin/VPS-MX &> /dev/null
 chmod +x /usr/bin/VPS-MX
 echo -ne '\e[97mCARGANDO:\033[1;37m\e[93m ############################## (100%)\r'
 echo -ne '\n'
